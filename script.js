411f7db50980638b96fe3a17295fe550827369d2
Chidi Usanga <chidiusanga@users.noreply.github.com>
2026-08-25 18:13:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -301,7 +301,7 @@ console.log(
    // REPLACED HERE TO MOVE "AWAITING WATER LEVEL DATA" DOWN
 
       wctx.save();
-      const overlayY = WH - 35;
+      const overlayY = WH - 100;
       wctx.fillStyle = 'rgba(180,20,20,.28)';
       wctx.strokeStyle = '#ff8080';
       wctx.lineWidth = 1;
