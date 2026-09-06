9829020d5b467639072c568e72eb90f4db3b47bd
Chidi Usanga <chidiusanga@users.noreply.github.com>
2026-09-06 23:19:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -737,7 +737,7 @@ if (!hasValidData(val)) {
           needleCircleSize:7, needleCircleOuter:true, needleCircleInner:false,
           colorNeedleCircleOuter:'#405060', colorNeedleCircleOuterEnd:'#607080',
           colorNeedleCircleInner:'#d8e8f4', colorNeedleCircleInnerEnd:'#d8e8f4',
-          fontValueSize:18,
+          fontValueSize:34,
         })).draw();
       }
     } catch(gaugeErr) {
