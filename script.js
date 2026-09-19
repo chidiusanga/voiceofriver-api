3b100b99ee523049d2322de5189dac513c1db5be
Chidi Usanga <chidiusanga@users.noreply.github.com>
2026-09-19 15:53:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -817,7 +817,11 @@ const sz = Math.max(
 
 
    const valTag = document.createElement('div');
-   valTag.className = 'gauge-value-tag';
+   
+   valTag.className =
+     gaugeType === 'linear-vertical'
+       ? 'gauge-value-tag thermometer-tag'
+       : 'gauge-value-tag';
    
    // Display text content only if there's a data value, otherwise display "Sensor Offline..." instead of just 000.00 or NaN.
 if (!hasValidData(val)) {
